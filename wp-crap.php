a69c1ffb57da6339ff8a717af7d09aee84deac4a
Andy <andy@parallelus.net>
2015-06-07 08:31:25 -0500

    Plugin file added.

---
+++ wp-crap.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: Customizer Remove All Plugin (CRAP)
+Plugin URI: https://github.com/parallelus/WP-CRAP
+Description: Removes the WordPress Customizer completely from your install.
+Version: 0.1
+Author: Andy Wilkerson, Jesse Petersen
+Author URI: http://para.llel.us, http://www.petersenmediagroup.com
+Text Domain: wp-crap
+Domain Path: /lang/
+
+Copyright 2015 Andy Wilkerson, Jesse Petersen.
+
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+*/
